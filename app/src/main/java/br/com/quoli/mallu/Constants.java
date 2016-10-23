a6bb3bf8dfe7329cc9791b9210141cdbe2562f17
Joao Santana <joaosantana.ti@gmail.com>
2016-10-22 22:30:46 -0200

    First commit

---
+++ app/src/main/java/br/com/quoli/mallu/Constants.java
@@ -0,0 +1,11 @@
+package br.com.quoli.mallu;
+
+/**
+ * Created by joao on 22/10/16.
+ */
+
+public class Constants {
+
+    public final String FIREBASE_DATABASE_URL = BuildConfig.FIREBASE_DATABASE_URL;
+    public final String FIREBASE_STORAGE_URL = BuildConfig.FIREBASE_STORAGE_URL;
+}
